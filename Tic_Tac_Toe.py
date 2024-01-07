253ca30b9358045fe0024d48700c58ab4e30d8e4
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-07 12:20:36 +0530

    Applied a Bandage for turn

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,11 +8,15 @@ SCREEN_HEIGHT = 399
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+i = 0
+computer_turn = False
 isrunning = True
 ispressed = False
 
 # List to store the positions of clicked circles
 clicked_circles = []
+# List to store the positions of computer's crosses
+computer_clicked_crosses = []
 
 while isrunning:
     for event in pygame.event.get():
@@ -38,12 +43,30 @@ while isrunning:
     # Draw the clicked circles
     for circle_pos in clicked_circles:
         pygame.draw.circle(screen, "white", circle_pos, 50, 5)
+        # i = 0
+
+    # Draw the computer's crosses
+    for cross_pos in computer_clicked_crosses:
+        pygame.draw.circle(screen, "red", cross_pos, 50, 5)
 
     # Draw the current circle if the mouse button is pressed
-    if ispressed:
+    if ispressed and not computer_turn:
+        # i = 0
         for rect in rects:
-            if rect.collidepoint(pygame.mouse.get_pos()):
+            if rect.collidepoint(pygame.mouse.get_pos()) and rect.center not in computer_clicked_crosses:
                 clicked_circles.append(rect.center)
+                computer_turn = True
+
+    # Computer's Turn
+    if computer_turn:
+        computer_turn = False   
+        available_rects = [rect for rect in rects if rect.center not in clicked_circles and rect.center not in computer_clicked_crosses]
+        if available_rects and i < 1:
+            chosen_rect = random.choice(available_rects)
+            computer_clicked_crosses.append(chosen_rect.center)
+            i += 1
+            
+    print(computer_clicked_crosses)
 
     pygame.display.flip()
 
