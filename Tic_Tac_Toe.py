6eb6001d6378b237cf23078f0afabc607b593892
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-08 17:47:37 +0530

    Made the turn based system bulletproof

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -8,23 +8,26 @@ SCREEN_HEIGHT = 399
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-i = 0
-computer_turn = False
+player_turn = True
 isrunning = True
 ispressed = False
 
-# List to store the positions of clicked circles
+# List to store the positions of clicked circles (player's moves)
 clicked_circles = []
 # List to store the positions of computer's crosses
 computer_clicked_crosses = []
+
+def check_winner():
+    # Add logic to check for a winner or a tie
+    pass
 
 while isrunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isrunning = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and player_turn:
             ispressed = True
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONUP and player_turn:
             ispressed = False
     
     screen.fill("black")
@@ -40,33 +43,33 @@ while isrunning:
     pygame.draw.line(screen, "white", (0, 133), (399, 133), 5)
     pygame.draw.line(screen, "white", (0, 266), (399, 266), 5)
 
-    # Draw the clicked circles
+    # Draw the clicked circles (player's moves)
     for circle_pos in clicked_circles:
         pygame.draw.circle(screen, "white", circle_pos, 50, 5)
-        # i = 0
 
     # Draw the computer's crosses
     for cross_pos in computer_clicked_crosses:
         pygame.draw.circle(screen, "red", cross_pos, 50, 5)
 
-    # Draw the current circle if the mouse button is pressed
-    if ispressed and not computer_turn:
-        # i = 0
+    # Player's Turn
+    if player_turn and ispressed:
         for rect in rects:
-            if rect.collidepoint(pygame.mouse.get_pos()) and rect.center not in computer_clicked_crosses:
+            if rect.collidepoint(pygame.mouse.get_pos()) and rect.center not in clicked_circles and rect.center not in computer_clicked_crosses:
                 clicked_circles.append(rect.center)
-                computer_turn = True
+                player_turn = False
+                ispressed = False
 
     # Computer's Turn
-    if computer_turn:
-        computer_turn = False   
+    if not player_turn:
+        # Implement logic for the computer's move
         available_rects = [rect for rect in rects if rect.center not in clicked_circles and rect.center not in computer_clicked_crosses]
-        if available_rects and i < 1:
+        if available_rects:
             chosen_rect = random.choice(available_rects)
             computer_clicked_crosses.append(chosen_rect.center)
-            i += 1
-            
-    print(computer_clicked_crosses)
+            player_turn = True
+
+    # Check for a winner or a tie
+    check_winner()
 
     pygame.display.flip()
 
