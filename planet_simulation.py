640e63b6a72b646d99dc20a12ea9b7935f55e6fd
Sahil Chhoker <chhokersahil435@gmail.com>
2024-02-17 17:04:09 +0530

    added venus

--- planet_simulation.py
+++ planet_simulation.py
@@ -55,8 +55,9 @@ def main():
 
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
 
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
 
-    planets = [sun, earth, mars, mercury]
+    planets = [sun, earth, mars, mercury, venus]
 
     # game loop 
     while run:
