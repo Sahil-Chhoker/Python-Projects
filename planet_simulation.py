1f5c4634a50c411135cce3fc42829bfa93bb880c
Sahil Chhoker <chhokersahil435@gmail.com>
2024-02-17 16:41:34 +0530

    added logic for screen

--- planet_simulation.py
+++ planet_simulation.py
@@ -1,8 +1,14 @@
 import pygame
 import math
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Simulation")
 
 class Planet():
-    pass
+    def __init__(self, x, y, radius, color, mass):
+        pass
 
 def main():
     pass
