34103abad1bf63bf79327c9cb3d25b992860ccce
Sahil Chhoker <chhokersahil435@gmail.com>
2024-02-17 16:43:31 +0530

    initalized necessary variables

--- planet_simulation.py
+++ planet_simulation.py
@@ -8,7 +8,18 @@ pygame.display.set_caption("Planet Simulation")
 
 class Planet():
     def __init__(self, x, y, radius, color, mass):
-        pass
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        self.orbit = []
+        self.sun = False
+        self.disrance_to_sun = 0
+
+        self.x_vel = 0
+        self.y_vel = 0
 
 def main():
     pass
