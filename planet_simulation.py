95009a8ce9627b939b8a3ff2cc2e311d1a6d6270
Sahil Chhoker <chhokersahil435@gmail.com>
2024-02-17 17:02:34 +0530

    added mercury and cleaned up the project

--- planet_simulation.py
+++ planet_simulation.py
@@ -2,14 +2,17 @@ import pygame
 import math
 pygame.init()
 
+# screen specs
 WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
+# colors
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 BLUE = (100, 149, 237)
 RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
 
 class Planet():
     AU = 149.6e6 * 1000
@@ -17,6 +20,7 @@ class Planet():
     SCALE = 250/AU # 1AU = 100 pixels
     TIMESTEP = 3600 * 24 # 1 day
 
+    # initilization 
     def __init__(self, x, y, radius, color, mass):
         self.x = x
         self.y = y
@@ -31,6 +35,7 @@ class Planet():
         self.x_vel = 0
         self.y_vel = 0
 
+    # drawing logic
     def draw(self, win):
         x = self.x * self.SCALE + WIDTH/2
         y = self.y * self.SCALE + HEIGHT/2
@@ -40,6 +45,7 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
+    # planets 
     sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
     sun.is_sun = True
 
@@ -47,8 +53,12 @@ def main():
 
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
 
-    planets = [sun, earth, mars]
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
 
+
+    planets = [sun, earth, mars, mercury]
+
+    # game loop 
     while run:
         clock.tick(60)
 
@@ -63,4 +73,7 @@ def main():
     
     pygame.quit()
 
-main()
+
+# calling the logic
+if __name__ == "__main__":
+    main()
