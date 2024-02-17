6248b5d5b1c6f26375a07339e170c15cad5a2d48
Sahil Chhoker <chhokersahil435@gmail.com>
2024-02-17 16:50:13 +0530

    added necessary values and a draw function

--- planet_simulation.py
+++ planet_simulation.py
@@ -7,6 +7,11 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 class Planet():
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 250/AU
+    TIMESTEP = 3600 * 24
+    
     def __init__(self, x, y, radius, color, mass):
         self.x = x
         self.y = y
@@ -20,6 +25,11 @@ class Planet():
 
         self.x_vel = 0
         self.y_vel = 0
+
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH/2
+        y = self.y * self.SCALE + HEIGHT/2
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
 
 def main():
     run = True
