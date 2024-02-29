d4a7fdf825b9722cf5e6cbeb2ed06d8db4ecd668
sahil-chhoker <chhokersahil435@gmail.com>
2024-02-29 22:07:57 +0530

    completed the game

--- algebra_practice_game.py
+++ algebra_practice_game.py
@@ -19,32 +19,39 @@ def generate_equations():
     return eq_1, eq_2
 
 def main():
-    eq_1, eq_2 = generate_equations()
-
-    print("One step equation is:")
-    pretty_print(eq_1)
-    print("\nTwo step equation is:")
-    pretty_print(eq_2)
-    print()
-
     while True:
-        guess = input("Guess the solutions of these problems, write in the form x,y: ")
-        try:
-            x_guess, y_guess = map(float, guess.split(","))
-        except ValueError:
-            print("Please enter two numbers separated by a comma.")
-            continue
+        eq_1, eq_2 = generate_equations()
 
-        solution_x = round(float(solve(eq_1)[0]), 1)
-        solution_y = round(float(solve(eq_2)[0]), 1)
+        print("One step equation is:")
+        pretty_print(eq_1)
+        print("\nTwo step equation is:")
+        pretty_print(eq_2)
+        print()
 
-        print(f"The actual answers are x: {solution_x} and y: {solution_y}")
+        while True:
+            guess = input("Guess the solutions of these problems, write in the form x,y: ")
+            try:
+                x_guess, y_guess = map(float, guess.split(","))
+            except ValueError:
+                print("Please enter two numbers separated by a comma.")
+                continue
 
-        if x_guess == solution_x and y_guess == solution_y:
-            print("You are right!!")
+            solution_x = round(float(solve(eq_1)[0]), 1)
+            solution_y = round(float(solve(eq_2)[0]), 1)
+
+            print(f"The actual answers are x: {solution_x} and y: {solution_y}")
+
+            if x_guess == solution_x and y_guess == solution_y:
+                print("You are right!!")
+                break
+            else:
+                print("You are wrong. Let's try another problem.")
+                break  # Exit the current loop to generate new equations
+
+        play_again = input("Do you want to play again? (yes/no): ")
+        if play_again.lower() != "yes":
+            print("Thank you for playing!")
             break
-        else:
-            print("You are wrong, try again")
 
 if __name__ == "__main__":
     main()
