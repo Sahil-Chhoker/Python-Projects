0a930fd4af0b7dcedb48d5a51cebfa163cf6e411
sahil-chhoker <chhokersahil435@gmail.com>
2024-03-08 10:47:22 +0530

    gray scale done

--- Detection Crop/main.py
+++ Detection Crop/main.py
@@ -4,4 +4,12 @@ from matplotlib import pyplot as plt
 
 # Watershed algorithm
 
-# step-1: convert to greyscale using otsu's binarization
+# step-1: convert to greyscale
+img_path = 'C:\MASTER FOLDER\Python-Projects\Detection Crop\dino_assets.png'
+sprite = cv.imread(img_path)
+assert sprite is not None, "file could not be read, check with os.path.exists()"
+
+gray = cv.cvtColor(sprite, cv.COLOR_BGR2GRAY)
+# ret, thrash = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)      // NO NEED FOR OTSU's BINARIZATION in this file
+cv.imshow("hi", gray)
+cv.waitKey(0)
