6d1af7a385cecdb393d5f9df2fa9d4b2e4e5fed7
sahil-chhoker <chhokersahil435@gmail.com>
2024-03-08 10:18:22 +0530

    more advance image cropper

--- Image Slitter/main.py
+++ Image Slitter/main.py
@@ -1,12 +1,19 @@
 import os
 import cv2 as cv
-sprite = cv.imread('C:\MASTER FOLDER\Python-Projects\Image Slitter\db35jhmr.png')
+
+image_path = 'C:\MASTER FOLDER\Python-Projects\Image Slitter\db35jhmr.png'
+save_path = 'C:\MASTER FOLDER\Python-Projects\Image Slitter'
+sprite = cv.imread(image_path)
 sprite_list = []
 
-for i in range(0, 1000, 100):
-    crop_sprite = sprite[0:1000, i:i+100]
+step = int(input("Write the step you want in the cropping : "))
+
+im_length, im_width, im_channel = sprite.shape
+
+for i in range(0, im_width, step):
+    crop_sprite = sprite[0:im_length, i:i+step]
     sprite_list.append(crop_sprite)
 
-os.chdir('C:\MASTER FOLDER\Python-Projects\Image Slitter')
+os.chdir(save_path)
 for i in range(len(sprite_list)):
     cv.imwrite(f"{i}.png", sprite_list[i])
